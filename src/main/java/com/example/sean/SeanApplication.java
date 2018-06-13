23df6330a1bc7de4136b5f2bb3b580e6bef74114
hatakes <332514900@qq.com>
2018-06-13 14:29:31 +0800

    first commit

---
+++ src/main/java/com/example/sean/SeanApplication.java
@@ -0,0 +1,12 @@
+package com.example.sean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeanApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/sean/SeanApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.sean;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SeanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
